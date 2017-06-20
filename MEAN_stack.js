3d1b63af05547f654436150915893dbf3faa1f26
Kristopher Merolla <kristopher.merolla@gmail.com>
2017-06-20 16:42:26 -0700

    updated notes to include deployment notes and general notes

--- MEAN_stack.js
+++ MEAN_stack.js
@@ -198,7 +198,7 @@
 			import { HttpService } from './../http.service';
 	// need to also INJECT into the component constructor:
 		main.component.ts
-			// before: 
+			// before:
 				constructor() { }
 			// after:
 				after: constructor(private _http: HttpService) { }
@@ -259,44 +259,106 @@
 			models/
 				product.js
 		server.js
-	// make a directory for our express app:
-		mkdir my-express-app
-			// go into the directory:
-				cd my-express-app
-					// initialize the node app:
-						npm init -y
-							// install packagaes:
-								npm install --save express
-								npm install --save body-parser
-								npm install --save mongoose
-							// create server file:
-								touch server.js
-								mkdir server
-									// go into server folder:
-										cd server
-											// create directories:
-												mkdir config
-													// add files in config (cd into config first):
-														cd config
-															touch mongoose.js
-															touch routes.js
-															cd ..
-												mkdir controllers
-													// add files:
-														cd controllers
-															touch products.js
-															cd ..
-												mkdir models
-													// add files:
-														cd models
-															touch product.js
-															cd ..
-															cd .. // back inside my-express-app dir
+	// To make an express app...
+		// make a directory for our express app:
+			mkdir my-express-app
+				// go into the directory:
+					cd my-express-app
+						// initialize the node app:
+							npm init -y
+								// install packagaes:
+									npm install --save express
+									npm install --save body-parser
+									npm install --save mongoose
+								// create server file:
+									touch server.js
+									mkdir server
+										// go into server folder:
+											cd server
+												// create directories:
+													mkdir config
+														// add files in config (cd into config first):
+															cd config
+																touch mongoose.js
+																touch routes.js
+																cd ..
+													mkdir controllers
+														// add files:
+															cd controllers
+																touch products.js
+																cd ..
+													mkdir models
+														// add files:
+															cd models
+																touch product.js
+																cd ..
+																cd .. // back inside my-express-app dir
 
 /////////////////////////////
 // 9) MongoDB and Mongoose //
 /////////////////////////////
 	// give collection names in LOWER CASE
+
+
+//////////////////////
+// A) General Notes //
+//////////////////////
+	// Data Binding:
+		// one-way bind:
+			[ngStyle]="red"
+		// two-way bind (banana in a box):
+			[( )]
+		// event listener:
+			( )
+	// Observeables:
+		// Subscribe
+		// Observables "live" in the services
+		// Behavior subject
+	// Architecture:
+		*ngIf
+		*ngFor
+	// Routing:
+		// Subscribe to the url (when url changes, you want to be updated)
+		// Actiated route
+	// Services:
+		ng g service http
+			// update your providers in:
+				app.module.ts
+		// Connection to the server (http requests)
+		// Any API request handled by service
+		// Don't store data in services (singleton)
+	// Forms:
+		// Front end validations
+			"errors should be displayed"
+		// Default is a "GET" request (so if you see the form info in the url you're doing something wrong!)
+	// Pipe filters:
+		{{ note.createdAt | date }} // SYNTAX NOT TURE
+		// Custom pipes:
+			ng g pipe
+				// write code for custom pipe <see documentation>
+
+/////////////////////////////////
+// B) Session (Angular Cookie) //
+/////////////////////////////////
+	// put a cookie on the user's browser
+	// must have a session KEY
+		// inspect element, see application, left side see cookies (to check)
+	// Install using npm:
+		npm install ngx-cookie --save
+			// add to app module:
+				import { CookieModule } from 'ngx-cookie';
+					// add to imports too:
+						imports: [ BrowserModule, CookieModule.forRoot() ],
+			// add:
+				import { CookieService } from 'ngx-cookie';
+					// add within app component constructor:
+						export class AppComponent { 
+							constructor(private _cookieService:CookieService){}
+
+							getCookie(key: string){
+								return this._cookieService.get(key);
+							}
+						}
 
 
 ////////////////////////////////////
@@ -345,7 +407,7 @@
 				// the '/' route will automatically point to index.html, so don't need to add that:
 					routes.js
 						app.get('*', (req, res, next)=>{
-							res.sendFile(path.resolve("./public/dist/index.html")); // SYNTAX MAY NOT BE CORRECT FOR THIS LINE
+							res.sendFile(path.resolve("./public/dist/index.html"));
 						});
 	// In your terminal in the express app directory (my-express-app) enter:
 		nodemon server.js
@@ -357,7 +419,115 @@
 ///////////////////
 // Y) Deployment //
 ///////////////////
-	// Using AWS...
+	// Make sure you have removed /dist from gitignore
+	// Change serverpaths to remove "localhost://"
+	// On the AWS site (in EC2) click:
+		"Launch Instance"
+			// Select free tier Ubuntu Server (assuming you want that):
+				"Ubuntu Server 16.04 LTS (HVM), SSD Volume Type"
+					"t2.micro"
+						"Review and Launch"
+							// Set the security settings:
+								"Edit Secutiry Groups"
+									// add the following
+										ssh 0.0.0.0, (Anywhere or myIP)
+										http 0.0.0.0 (Anywhere)
+										https 0.0.0.0 (Anywhere, or dont set it)
+											// Save and launch!
+												"Launch" 
+		// After you launch, you'll be asked to either create a key or use a key existing
+			// Very important, never save your key to anywhere that is a git repository!!!
+				"Launch Instance"
+					// If you need to setup a new AWS key:
+						Download a .pem key from AWS
+						Move the .pem file to an appropriate folder on your system
+						Change user permission on your .pem  file using the command: chmod 400 {{mypem}}.pem
+	// Now your instance is up and running:
+		"View Instances"
+			// In your TERMINAL, navigate to the location where you stored your .pem key
+				// In your browser on the AWS site, with your instance selected click:
+					"Connect"
+						// you'll see some commands, copy them, then back in the TERMINAL paste them (example):
+							chmod 400 {{yourKeyName}}.pem
+							ssh -i "{{yourKeyName}}.pem" ubuntu@ec2-{yourAWS.ip}.us-west-1.compute.amazonaws.com
+								// At this point, you should be in your ubuntu machine, TERMINAL looks like:
+									ubuntu@ip-{{yourAWS.ip}}:-$
+	// In your virtual machine terminal (ubuntu@ip-{{yourAWS.ip}}:-$), install some packages (1 at a time):
+		sudo apt-get update
+		sudo apt-get install -y build-essential openssl libssl-dev pkg-config
+		sudo apt-get install -y nodejs nodejs-legacy
+		sudo apt-get install npm
+		sudo npm cache clean -f
+		sudo npm install -g n
+		sudo n stable
+		sudo apt-get install nginx
+		sudo apt-get install git
+		// go into the "www" directory (you may need to create it with "sudo mkdir /var/www"):
+			cd /var/www
+				// Your terminal window should look like this now:
+					ubuntu@ip-{{yourAWS.ip}}:/var/www$
+						// For the next step, you need to alraedy have your project on GitHub (google if unsure)
+						// Clone your git repository in to the local machine (example):
+							"sudo git clone https://github.com/{{yourGitHubUsername}}/{{yourGitRepository}}.git"
+	// In your vitrual machine terminal, time to setup NGINX:
+		cd /etc/nginx/sites-available
+			// Your terminal window should look like this now:
+				ubuntu@ip-{{yourAWS.ip}}:/etc/nginx/sites-available$
+					// enter VIM:
+						sudo vim {{project_name}}
+							// insert into the file (google if unsure) this:
+								server {
+									listen 80;
+									location / {
+										proxy_pass http://{{yourAWS.ip}}:{{portNumber}};
+										proxy_http_version 1.1;
+										proxy_set_header Upgrade $http_upgrade;
+										proxy_set_header Connection 'upgrade';
+										proxy_set_header Host $host;
+										proxy_cache_bypass $http_upgrade;
+									}
+								}
+			// remove defauls:
+				sudo rm default
+			// Create a symbolic link from sites-enabled to sites available:
+				sudo ln -s /etc/nginx/sites-available/{{project_name}} /etc/nginx/sites-enabled/{{project_name}}
+			// Remove the defaults from /etc/nginx/sites-enabled/:
+				cd /etc/nginx/sites-enabled/
+					sudo rm default
+	// Install P2M globally:
+		sudo npm install pm2 -g
+			// Try some stuff with P2M:
+				cd /var/www/{{project_name}}
+					pm2 start server.js
+					pm2 stop 0
+					pm2 restart 0
+					sudo service nginx reload && sudo service nginx restart
+			// You may have dependencies to install, so within the project directory:
+				sudo npm install
+					// if you're doing a full MEAN app, you'll also need to go into the public directory:
+						sudo npm install
+	// Lastly, need to setup MongoDB:
+		sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+			// below command is ONE LINE (return'd for space at the pipe | )
+				echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.4 multiverse" 
+				 | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+		// Integrate Mongo:
+			sudo apt-get update
+		// Install Mongo:
+			sudo apt-get install -y mongodb-org
+			// -or- (if the above doesnt work)
+			sudo apt-get install -y mongodb
+		// Starto Mongo:
+			sudo mongod
+				// Check for an error code. It might ask you to build a `/data/db:
+					cd /
+					sudo mkdir data
+					sudo mkdir data/db
+			// When you can see Mongo start up, close it (ctrl+c) and use pm2 so we can keep terminal up:
+				sudo pm2 start mongod
+
+
+
 
 
 
