b19666432df498b9da0e3c2acd2093e8c2ec299b
Kristopher Merolla <kristopher.merolla@gmail.com>
2017-07-27 17:15:34 -0700

    initial commit, javascript notes and functions document for helpful tips and things to remember about javascript

---
+++ javascript_notes_and_functions.js
@@ -0,0 +1,20 @@
+//###############################################//
+//## JavaScript Notes, Functions, Helpful Tips ##//
+//###############################################//
+
+
+///////////////////
+// General Notes //
+///////////////////
+	// Print to the console
+	console.log("hello world")
+
+//////////////////////////////
+// Viewing your Code Output //
+//////////////////////////////
+	// Node (Node.js) is a great way to visualize your javascript code in real time
+		// From within the proper directory in terminal
+		nodemon filename.js
+
+	// Add HTML code inside a <script></script> tag and save the file as "filename.html" to view on browser console
+		// Use Chrome!  Right click to inspect element to view the console
