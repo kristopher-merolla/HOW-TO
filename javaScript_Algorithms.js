936c5395173f2b891fef32b1eed35b9c4e0950f8
Kristopher Merolla <kristopher.merolla@gmail.com>
2017-08-28 18:39:38 -0700

    Initial commit, JavaScript Algorithms

---
+++ javaScript_Algorithms.js
@@ -0,0 +1,5 @@
+// The purpose of this file is to write and test JavaScript functions for various algorithm challenges
+// Created 8/28/2017 - Kristopher Merolla 
+/* ------------------------------------------ */
+
+console.log("hello JavaScript algorithms!");
